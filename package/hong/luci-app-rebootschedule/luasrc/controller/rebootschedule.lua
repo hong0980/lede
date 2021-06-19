da76fe03da2ff0281d200ed5bd8a7ebf0da0b04c
hong0980 <hong0980@qq.com>
2021-06-19 16:45:18 +0800

    update@2021-06-19-16-45

--- package/hong/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua
+++ package/hong/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua
@@ -1,13 +1,15 @@
 module("luci.controller.rebootschedule", package.seeall)
 function index()
-	if not nixio.fs.access("/etc/config/rebootschedule") then
-		return
-	end
-	
-	local page
-	page = entry({"admin", "system", "rebootschedule"}, cbi("rebootschedule"), "Reboot schedule", 88)
-	page.dependent = true
+	if not nixio.fs.access("/etc/config/rebootschedule") then return end
+	entry({"admin", "services", "rebootschedule"},
+		alias("admin", "services", "rebootschedule", "general"),
+		_("定时任务 Plus+"), 10)
+
+	entry({"admin", "services", "rebootschedule", "general"},
+		cbi("rebootschedule/general"),
+		_("时间设置"), 20).leaf = true
+
+	entry({"admin", "services", "rebootschedule", "crontab"},
+		cbi("rebootschedule/crontab"),
+		_("crontab文件"), 30).leaf = true
 end
-
-
-
